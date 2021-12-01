b912300c729ba56ff38f33e6b7b25e84b7b1b199
Mate Krenner <matthew.krenner@gmail.com>
2021-12-02 00:17:54 +0100

    v1.3

--- Project/index.js
+++ Project/index.js
@@ -1,8 +1,13 @@
-const overlay = {
-    on: () => {
-        document.getElementById("overlay").style.display = "block";
-    },
-    off: () => {
-        document.getElementById("overlay").style.display = "none";
-    },
-}
+window.onscroll = function() {myFunction()};
+
+const header = document.getElementById("header");
+
+const sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+} 
